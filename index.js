3f078956a8821e32fc7d86d3532e3d46ee3e7670
Miha1986nijniinovgorod <Miha1986nijniinovgorod@outlook.com>
2022-09-24 10:46:21 +0300

    первое занятие

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("helloWorld")
+const ourPlanet='земля';
+console.log (ourPlanet);
+let timyou='11';
+console.log (timyou);
+let numberList='more';
+console.log (numberList);
+let kardORcash='kard';
+console.log (kardORcash);
+const ferstName='miha';
+console.log (ferstName);
+const secondName='Niha';
+console.log (secondName);
+const fatherName='vasil';
+console.log (fatherName);
